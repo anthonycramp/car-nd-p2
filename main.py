5929d6b6e9f50730e2bc75005482b69d85bcf950
Anthony Cramp <anthony.cramp@gmail.com>
2020-05-30 21:40:43 +0930

    Removed some dead code, moved module scope variables into a function, added main check to call this function.

--- main.py
+++ main.py
@@ -4,23 +4,6 @@ import numpy as np
 import proj2
 import os
 
-test_images_dir = "test_images"
-test_image_filenames = [
-    "straight_lines1.jpg",
-    "straight_lines2.jpg",
-    "test1.jpg",
-    "test2.jpg",
-    "test3.jpg",
-    "test4.jpg",
-    "test5.jpg",
-    "test6.jpg",
-]
-output_dir = "output_images"
-
-source_image_filename = test_image_filenames[0]
-source_image_path = os.path.join(test_images_dir, source_image_filename)
-image = cv2.imread(source_image_path)
-
 def create_image_filename(source_filename, dest_filename_suffix):
     root, ext = os.path.splitext(source_filename)
     return "{}-{}{}".format(root, dest_filename_suffix, ext)
@@ -32,14 +15,32 @@ def write_image(output_dir, source_image_filename, dest_filename_suffix, image):
 def write_binary_image(output_dir, source_image_filename, dest_filename_suffix, image):
     write_image(output_dir, source_image_filename, dest_filename_suffix, image * 255)
 
-for source_image_filename in test_image_filenames:
-    img = cv2.imread(os.path.join(test_images_dir, source_image_filename))
-    imgs = proj2.process_image(img)
+def run():
+    test_images_dir = "test_images"
+    test_image_filenames = [
+        "straight_lines1.jpg",
+        "straight_lines2.jpg",
+        "test1.jpg",
+        "test2.jpg",
+        "test3.jpg",
+        "test4.jpg",
+        "test5.jpg",
+        "test6.jpg",
+    ]
+    output_dir = "output_images"
 
-    write_image(output_dir, source_image_filename, "gray", imgs["gray"])
-    write_image(output_dir, source_image_filename, "s", imgs["hls"][:,:,2])
-    write_binary_image(output_dir, source_image_filename, "gradx", imgs["gradx"])
-    write_binary_image(output_dir, source_image_filename, "grady", imgs["grady"])
-    write_binary_image(output_dir, source_image_filename, "gradm", imgs["gradm"])
-    write_binary_image(output_dir, source_image_filename, "gradd", imgs["gradd"])
-    write_binary_image(output_dir, source_image_filename, "gradc", imgs["gradc"])
+    for source_image_filename in test_image_filenames:
+        img = cv2.imread(os.path.join(test_images_dir, source_image_filename))
+        imgs = proj2.process_image(img)
+
+        write_image(output_dir, source_image_filename, "gray", imgs["gray"])
+        write_image(output_dir, source_image_filename, "s", imgs["hls"][:,:,2])
+        write_binary_image(output_dir, source_image_filename, "gradx", imgs["gradx"])
+        write_binary_image(output_dir, source_image_filename, "grady", imgs["grady"])
+        write_binary_image(output_dir, source_image_filename, "gradm", imgs["gradm"])
+        write_binary_image(output_dir, source_image_filename, "gradd", imgs["gradd"])
+        write_binary_image(output_dir, source_image_filename, "gradc", imgs["gradc"])
+
+if __name__ == "__main__":
+    run()
+
